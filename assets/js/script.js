41d0f5f97b033c2c46c1362858c9fb40d895b8ab
Angelo Fallaria <ba.fallaria@gmail.com>
2021-11-29 09:45:43 +0800

    feat: first commit

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function operate(operator, numbers) {
+    return operator(...numbers);
+}
---
+++ assets/js/script.ts
@@ -0,0 +1,19 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+function subtract(a: number, b: number): number {
+  return a - b;
+}
+
+function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+function operate(operator: Function, numbers: number[]): number {
+  return operator(...numbers);
+}
